ddb0fc8f522858c0fbe00f79aea4b9ac494e3260
Brayan054 <rayan.benmehal@outlook.fr>
2017-11-05 16:28:15 +0100

    main programe

--- calculator/main.c
+++ calculator/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+
+   struct student {
+    char nom[50];
+    char prenom[50];
+    int age;}
+
+  int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int i;
+    struct student tab[3];
+    printf("Hello meeen!\n");
+
+   for(i=1;i<=3;i++);{
+   printf("donner le nom,prenom,age\n");
+   scanf("%s%s%d",&tab[i].nom,&tab[i].prenom,&tab[i].age);
+}
+   for (i=1;i<=3;i++);{
+
+   printf("etudiant num %d,nom:%s,prenom:%s,age%d\n",i,tab[tab].nom,tab[i].prenom,tab[i].age);
+   }
 }
